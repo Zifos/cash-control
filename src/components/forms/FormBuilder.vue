f2565e8e27e438b81fa4479369c5cf832637b976
feco <hola@fecocode.com>
2020-12-10 21:03:07 -0300

    add actions forms styles

--- src/components/forms/FormBuilder.vue
+++ src/components/forms/FormBuilder.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="form__wrapper">
     <div class="form__wrapper__actions">
-      <button @click="openLauncherEntitiesDrawer">Cancelar</button>
-      <button>Guardar</button>
+      <button @click="openLauncherEntitiesDrawer" class="form__wrapper__actions-cancel">CANCELAR</button>
+      <button class="form__wrapper__actions-save">GUARDAR</button>
     </div>
     <div class="form__wrapper__content">
       <input type="number"/>
@@ -37,6 +37,21 @@ export default {
     &__actions {
       display: flex;
       justify-content: space-between;
+      button {
+        height: 2rem;
+        font-size: .9rem;
+        border-radius: .2rem;
+      }
+      &-cancel {
+        color: #fa5252;
+        background: #fff5f5;
+        border: 1px solid #fa5252;
+      }
+      &-save {
+        color: #40c057;
+        background: #ebfbee;
+        border: 1px solid #40c057;
+      }
     }
   }
 </style>
